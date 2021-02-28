84023f86e6fffc6609b501c3883d2c93696975ec
red-pand-web <readex934@gmail.com>
2021-02-28 21:17:16 +0300

    main video finished, the first video block started

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded", function () {
+   const swiper = new Swiper('.swiper-container.swiper-container__videoLine1', {
+
+      slidesPerView: 4,
+      spaceBetween: 13,
+      navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+      },
+   });
 
 });
